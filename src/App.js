dcd1708052e68ee2bc229510b35ab9dd9a15a8d0
ateeb <syedateebulislam@gmail.com>
2021-05-23 22:40:29 +0530

    JSX demo in Hello.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet';
 import Welcome from './Components/Welcome';
+import Hello from './Components/Hello'
 
 function App() {
   return (
     <div className="App">
 
       <Greet/>
-      <Welcome/>
+      <Welcome/>  
+      <Hello/>
+
     </div>
   );
 }
